89588a327c70a0b51c84a77afb3f15e9bf29b946
Stephen Park <stephen.park@live.com>
2018-04-02 18:43:54 -0400

    changes to the js w/ comment

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
-let navbar = document.getElementById("navbar");
+// Got the code from the W3 website... https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+// i modified the navbar to change colors when highlighted, rounded the button more and added a action to each button.
+
+// this is a function that works on scroll. its sets the navbar/#header(id) to sticky once passed a certain part of the page and stickys it as you scroll down the page.
+
+let header = document.getElementById("header");
 
 let sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    header.classList.add("sticky")
   } 
   else {
-    navbar.classList.remove("sticky");
+    header.classList.remove("sticky");
   }
 }
